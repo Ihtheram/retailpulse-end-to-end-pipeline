cb56b48b2a89b97e699c9598acb3ab5f176cc62e
Ihtheram <ihtheram.chowdhury@outlook.com>
2026-04-27 06:50:53 -0400

    Set up S3 bucket through Terraform
    * Defined aws provider in providers.tf with aws_region which is defined in variables.tf
    * Defined resource for S3 bucket in main.tf with bucket_name which is defined in variables.tf
    * Ran command `terraform plan -out=tfplan` which generated and saved plan in tfplan
    * Ran `terraform apply tfplan` to apply the saved tfplan and generate the defined S3 bucket in aws

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────
+# AWS Variables
+# ──────────────────────────────────────────────
+
+variable "aws_region" {
+  description = "AWS region where the S3 bucket will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Globally unique name for the S3 raw data landing bucket"
+  type        = string
+  default     = "retailpulse-raw-data-landing"
+}
